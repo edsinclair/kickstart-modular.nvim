4d7f9c2cdefda66e0825e8610b1ce4cd2af1f97e
Eirik Dentz Sinclair <eirikdentz@gmail.com>
2025-04-21 15:42:30 +0200

    Customize vim-rails alternate file behavior for request specs

--- lua/custom/plugins/vim-rails.lua
+++ lua/custom/plugins/vim-rails.lua
@@ -1,3 +1,13 @@
 return {
-  'tpope/vim-rails'
+  'tpope/vim-rails',
+  config = function()
+    vim.g.rails_projections = {
+      ["app/controllers/*_controller.rb"] = {
+        test = { "spec/requests/{}_spec.rb" },
+      },
+      ["spec/requests/*_spec.rb"] = {
+        alternate = { "app/controllers/{}_controller.rb" },
+      }
+    }
+  end
 }
